8048762c2f1b3fda45b588b3129df398ee2263e7
red root <boney.warikozi@gmail.com>
2025-05-29 22:23:46 -0700

    changing the speed in profile

--- resume.js
+++ resume.js
@@ -707,12 +707,12 @@ var initProgram = setTimeout(function(){
             if(i < $habitpara.length){
                 setTimeout(function() {
                     initHabitPara( i + 1);
-                }, 3500);
+                }, 15);
             }else{
                 $('#habitpara').removeClass("cursor");
                 setTimeout(function (){
                    initMyName(0);
-                }, 15000);
+                }, 150);
             }
         }
 
